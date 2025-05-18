2ee70325380912ec7083607f3bda5804aacf3989
MichaelAngeloTheFirst <siemic13@gmail.com>
2025-05-18 21:25:04 +0200

    sending messages + zwc config

--- message_hiding.py
+++ message_hiding.py
@@ -1,1 +1,30 @@
+import pandas as pd
 
+
+ZWC = ("\u200c", "\u202c", "\u202d", "\u200e")
+zw_space = "\u200b"
+zw_non_joiner = "\u200c"
+zw_joiner = "\u200d"
+zw_no_break_space = "\ufeff"
+
+
+# function to import unique array
+def import_unique_array(file_path):
+    try:
+        df = pd.read_csv(file_path)
+        unique_array = df.iloc[:, 0].unique()
+        # add index to unique array
+
+        return unique_array
+    except Exception as e:
+        print(f"Error reading file: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    # Example usage
+    secret_message = "This is a hidden message."
+    carrier_message = "This is the visible part of the message."
+
+    message = f"Hello,{zw_joiner} World !"
+    print("Original message:", message)
--- sending_mail.py
+++ sending_mail.py
@@ -2,20 +2,36 @@ import smtplib
 import ssl
 import os
 from dotenv import load_dotenv
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 load_dotenv()
+
+zw_joiner = "\u200d"
+hidden_message = f"Hello World Hidden{zw_joiner}Text"
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = os.getenv("SENDER_EMAIL")  # Enter your address
 receiver_email = os.getenv("RECIPIENT_EMAIL")  # Enter receiver address
-password = os.getenv("APP_PASSWD")  # input("Type your password and press enter: ")
-message = """\
-Subject: Hi there
+password = os.getenv("APP_PASSWD")
 
-This message is sent from Python."""
+# Construct the email
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = "Hi there"
 
+# Email body with hidden content
+body = f"Hello,\nThis message is sent from Python.\n{hidden_message}"
+message.attach(MIMEText(body, "plain"))
+
+# Send email
 context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+try:
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+    print("Email sent successfully.")
+except Exception as e:
+    print(f"Error sending email: {e}")
